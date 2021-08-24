cbd7bd353493d6e141b84850da87e36c50fc75e7
yuoran <82818078+yuoran@users.noreply.github.com>
2021-08-24 09:03:25 +0800

    Update NiuKe.py

--- NiuKe.py
+++ NiuKe.py
@@ -1,6 +1,6 @@
 # -*- codeing =utf-8 -*-
 # @Time :2021/8/23 23:04
-# @Author :张玉洁
+# @Author :***
 # @File :NiuKe.py
 # @Software: PyCharm
 import time
@@ -10,7 +10,7 @@ from lxml import etree
 while True:
     time_now = time.strftime("%M:%S",time.localtime())
     if time_now == "30:10":
-        conn = pymysql.connect(host='127.0.0.1', user='root', password='zyj191702', port=3306, database='热搜排行榜数据库')
+        conn = pymysql.connect(host='127.0.0.1', user='root', password='*****', port=3306, database='热搜排行榜数据库')
         cursor = conn.cursor()
 
         headers = {
